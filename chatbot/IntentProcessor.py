d5a3c4b36061b0f688ff258a7aa8923575923953
fotinitzmn <fotinitzmn@yahoo.com>
2025-05-19 13:04:54 +0300

    Update code

--- chatbot/IntentProcessor.py
+++ chatbot/IntentProcessor.py
@@ -6,13 +6,13 @@ class IntentProcessor:
     def __init__(self,intents_file):
         with open(intents_file,encoding='utf-8') as file:
             data = json.load(file)
-            self.intents = data["intents"] if isinstance(data,dict) and "intents" in data else data
+            self._intents = data["intents"] if isinstance(data,dict) and "intents" in data else data
 
     def get_patterns_and_labels(self):
         patterns = []
         labels = []
 
-        for intent_data in self.intents:
+        for intent_data in self._intents:
             intent = intent_data["tag"]
             for pattern in intent_data["patterns"]:
                 patterns.append(pattern)
@@ -20,14 +20,14 @@ class IntentProcessor:
         return patterns,labels
     
     def get_response(self,intent):
-        for intent_data in self.intents:
+        for intent_data in self._intents:
             if intent_data["tag"] == intent:
                 return random.choice(intent_data["responses"])
         return self.suggest_question()
 
     def suggest_question(self):
         sample_patterns = []
-        for intent_data in self.intents:
+        for intent_data in self._intents:
             if intent_data["patterns"]:
                 sample_patterns.append(random.choice(intent_data["patterns"]))
         if sample_patterns:
